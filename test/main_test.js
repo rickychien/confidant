a1981765b13e9da208387f2b914c96ba5480d248
gaye <gareth.aye@gmail.com>
2015-06-29 11:03:25 -0400

    Cleanup end-to-end test

--- test/main_test.js
+++ test/main_test.js
@@ -1,31 +1,41 @@
+let chai = require('chai');
+let chaiAsPromised = require('chai-as-promised');
 let exec = require('mz/child_process').exec;
-let expect = require('chai').expect;
-let fs = require('fs');
+let fs = require('mz/fs');
 let main = require('../src/main');
 
-let dir = `${__dirname}/fixtures/config_filter`;
-let files = [
-  `${dir}/build.ninja`,
-  `${dir}/random-copy`,
-  `${dir}/one/random-copy`,
-  `${dir}/other/random-copy`
+chai.use(chaiAsPromised);
+let expect = chai.expect;
+
+let testCases = [
+  {
+    name: 'basic',
+    dir: `${__dirname}/fixtures/config_filter`,
+    verify: function() {
+      let dir = this.dir;
+      return Promise.all(
+        [
+          `${dir}/build.ninja`,
+          `${dir}/random-copy`,
+          `${dir}/one/random-copy`,
+          `${dir}/other/random-copy`
+        ].map(path => expect(fs.exists(path)).to.eventually.be.true)
+      );
+    }
+  }
 ];
 
 suite('main', function() {
-  setup(function() {
-    files.map(file => {
-      try {
-        fs.unlinkSync(file);
-      } catch (error) {
-        // It's fine if it doesn't exist.
-      }
+  testCases.forEach(testCase => {
+    test(testCase.name, function() {
+      let dir = testCase.dir;
+      let execOpts = { cwd: dir, env: process.env };
+      return exec('ninja -t clean', execOpts)
+      .then(() => fs.unlink(`${dir}/build.ninja`))
+      .catch(() => { /* will fail if state is already clean */ })
+      .then(() => main(dir))
+      .then(() => exec('ninja', execOpts))
+      .then(testCase.verify.bind(testCase));
     });
-
-    return main(dir)
-    .then(() => exec('ninja', { cwd: dir, env: process.env }));
-  });
-
-  test('should write build.ninja', function() {
-    files.forEach(file => expect(fs.existsSync(file)).to.be.true);
   });
 });
